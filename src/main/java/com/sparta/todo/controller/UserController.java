ac13c1a38bd59781806d031f2b79183837293035
oneseel <seno1006@gmail.com>
2023-11-15 17:01:49 +0900

    로그인 API 구현

--- src/main/java/com/sparta/todo/controller/UserController.java
+++ src/main/java/com/sparta/todo/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.sparta.todo.controller;
 
+import com.sparta.todo.dto.LoginRequestDto;
 import com.sparta.todo.dto.SignupRequestDto;
 import com.sparta.todo.entity.User;
 import com.sparta.todo.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +20,18 @@ public class UserController {
 
     private final UserService userService;
 
-    // 회원가입
+    // 회원 가입
     @PostMapping("/signup")
     public ResponseEntity<User> signup(@RequestBody SignupRequestDto requestDto) {
         User user = userService.signup(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    // 로그인
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequestDto requestDto, HttpServletResponse res) {
+        String login = userService.login(requestDto, res);
+        return ResponseEntity.ok().body(login);
     }
 
 }
--- src/main/java/com/sparta/todo/service/UserService.java
+++ src/main/java/com/sparta/todo/service/UserService.java
@@ -1,8 +1,11 @@
 package com.sparta.todo.service;
 
+import com.sparta.todo.dto.LoginRequestDto;
 import com.sparta.todo.dto.SignupRequestDto;
 import com.sparta.todo.entity.User;
+import com.sparta.todo.jwt.JwtUtil;
 import com.sparta.todo.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -17,6 +20,9 @@ public class UserService {
 
     private final PasswordEncoder passwordEncoder;
 
+    private final JwtUtil jwtUtil;
+
+    // 회원 가입
     public User signup(SignupRequestDto requestDto) {
         String username = requestDto.getUsername();
         String password = passwordEncoder.encode(requestDto.getPassword());
@@ -24,14 +30,36 @@ public class UserService {
         // 회원 중복 확인
         Optional<User> checkUsername = userRepository.findByUsername(username);
         if (checkUsername.isPresent()) {
-            throw new IllegalArgumentException("중복된 사용자가 존재합니다.");
+            throw new IllegalArgumentException("중복된 회원이 존재합니다.");
         }
 
-        // 사용자 등록
+        // 회원 등록
         User user = new User(username, password);
 
         User saveUser = userRepository.save(user);
 
         return saveUser;
+    }
+
+    // 로그인
+    public String login(LoginRequestDto requestDto, HttpServletResponse res) {
+        String username = requestDto.getUsername();
+        String password = requestDto.getPassword();
+
+        // 회원 중복 확인
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new IllegalArgumentException("등록된 회원이 존재합니다.")
+        );
+
+        // 비밀번호 확인
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        // JWT 생성 및 쿠키에 저장 후 Response 객체에 추가
+        String token = jwtUtil.createToken(user.getUsername());
+        jwtUtil.addJwtToCookie(token, res);
+
+        return token;
     }
 }
